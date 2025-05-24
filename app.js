ed3793ce49dedec338cec2bac6cc6738d903d4ca
DiegoIglesias53 <iglesiasdiego53@gmail.com>
2025-05-23 23:01:33 -0600

    ya

--- app.js
+++ app.js
@@ -179,5 +179,5 @@ socket.on('webrtc-candidate', ({ to, candidate }) => {
 
 // Arrancar servidor
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+server.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`);
 
